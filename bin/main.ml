a07536a6c527c5cd2d8acc953328b0ba06b940d7
Eric Zhang <ericbkzhang@gmail.com>
2026-07-24 15:30:36 -0400

    Read and print user-given age
    It is necessary for judging how much you aged on your trip
    and whether you will likely survive it in the first place.

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,4 @@
-let () = print_endline "Hello, World!"
+let s = print_string "Greetings, traveler.
+Enter your your age as a decimal number of years (e.g., 3.14): ";
+    float_of_string (read_line ()) in
+    print_float s;
