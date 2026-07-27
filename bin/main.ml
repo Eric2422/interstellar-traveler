005437d145b88df3df312ab0aa30a86ff31d85c8
Eric Zhang <ericbkzhang@gmail.com>
2026-07-27 00:12:24 -0400

    Update prompt for ship speed

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,6 @@ Enter your your age as a decimal number of years (e.g., 3.14): ";
     float_of_string (read_line ()) in
     print_float age;
 
-    let speed = print_string "Greetings, traveler.
-    Enter your your age as a decimal number of years (e.g., 3.14): ";
-        float_of_string (read_line ()) in
-        print_float speed;
+let speed = print_string "Enter the maximum speed of your spaceship in meters per second (m/s): ";
+    float_of_string (read_line ()) in
+    print_float speed;
