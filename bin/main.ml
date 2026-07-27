7d662e91263a75b34eabcce9ce443d199d4b3857
Eric Zhang <ericbkzhang@gmail.com>
2026-07-27 00:30:33 -0400

    Format main file

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,14 @@
-let age = print_string "Greetings, traveler.
-Enter your your age as a decimal number of years (e.g., 3.14): ";
-    float_of_string (read_line ()) in
-    print_float age;
+let age =
+  print_string
+    "Greetings, traveler.\n\
+     Enter your your age as a decimal number of years (e.g., 3.14): ";
+  float_of_string (read_line ())
+in
+print_float age;
 
-let speed = print_string "Enter the maximum speed of your spaceship in meters per second (m/s): ";
-    float_of_string (read_line ()) in
-    print_float speed;
+let speed =
+  print_string
+    "Enter the maximum speed of your spaceship in meters per second (m/s): ";
+  float_of_string (read_line ())
+in
+print_float speed
