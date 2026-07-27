baf9d0ee3beb1fb2bdf7b8430a217de2e11683c9
Eric Zhang <ericbkzhang@gmail.com>
2026-07-26 23:59:15 -0400

    Ask for traveler speed

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,9 @@
-let s = print_string "Greetings, traveler.
+let age = print_string "Greetings, traveler.
 Enter your your age as a decimal number of years (e.g., 3.14): ";
     float_of_string (read_line ()) in
-    print_float s;
+    print_float age;
+
+    let speed = print_string "Greetings, traveler.
+    Enter your your age as a decimal number of years (e.g., 3.14): ";
+        float_of_string (read_line ()) in
+        print_float speed;
