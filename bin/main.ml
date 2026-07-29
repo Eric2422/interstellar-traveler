556c9d02a6df9c08a3da0d3df5c0628a7b79409e
Eric Zhang <ericbkzhang@gmail.com>
2026-07-29 08:08:34 -0400

    Set indent to 4 spaces

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,13 @@
 let () =
-  if Array.length Sys.argv < 2 then raise (Failure "Missing input filepath");
+    if Array.length Sys.argv < 2 then raise (Failure "Missing input filepath");
 
-  let json = Yojson.Basic.from_file Sys.argv.(1) in
+    let json = Yojson.Basic.from_file Sys.argv.(1) in
 
-  let open Yojson.Basic.Util in
-  let age = json |> member "age" |> to_float in
-  let max_speed = json |> member "max_speed" |> to_float in
+    let open Yojson.Basic.Util in
+    let age = json |> member "age" |> to_float in
+    let max_speed = json |> member "max_speed" |> to_float in
 
-  print_endline "Greetings, traveler.";
-  print_endline "Provided information:";
-  Printf.printf "\tAge: %f years" age;
-  Printf.printf "\tMaximum speed: %f m/s" max_speed
+    print_endline "Greetings, traveler.";
+    print_endline "Provided information:";
+    Printf.printf "\tAge: %f years" age;
+    Printf.printf "\tMaximum speed: %f m/s" max_speed
