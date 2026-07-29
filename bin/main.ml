002460479323a4b91b887933765657b5f58c1faa
Eric Zhang <ericbkzhang@gmail.com>
2026-07-29 18:39:02 -0400

    Print speed in both *c* and meters per second

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,5 @@
+let c = 299789458
+
 let () =
     if Array.length Sys.argv < 2 then raise (Failure "Missing input filepath");
 
@@ -11,5 +13,7 @@ let () =
     print_endline "Greetings, traveler.";
     print_endline "Provided information:";
     Printf.printf "    Age: %f years\n" age;
-    Printf.printf "    Maximum speed: %f m/s\n" max_speed;
+    Printf.printf "    Maximum speed: %f c (%f m/s)\n"
+      (max_speed /. float c)
+      max_speed;
     Printf.printf "    Proper mass: %f kg\n" proper_mass
